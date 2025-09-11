0e80bd486dcb8fab5034252d9821d93888d2d77f
dahirfara <dahir.meisour@gmail.com>
2025-09-11 15:50:18 +0300

    Added the unbilled event apis

--- mgs_unbilled_meter_event/controller/controllers.py
+++ mgs_unbilled_meter_event/controller/controllers.py
@@ -4,14 +4,68 @@ from odoo import http
 from odoo.http import request
 import werkzeug.wrappers as wrap
 
-# class MgsNecsomAddons(http.Controller):
-#     @http.route('/mgs/logout', type="http",auth='none')
-#     def logout(self, **kw):
-#         request.session.logout()
-#         return wrap.Response(
-#                 status=200,
-#                 content_type="application/json; charset=utf-8",
-#                 headers=[("Cache-Control", "no-store"), ("Pragma", "no-cache")],
-#                 response=json.dumps(  {
-#             "msg":"Successfully logged out"
-#         }))
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class Mgs_Unbilled_Meter_Event_Controller(http.Controller):
+    
+    
+    @http.route('/billingApi/unbilledmeterevent/', type='json', auth='user', methods=['POST'], csrf=False)
+    def create_unbilled_meter_event(self, **kw):
+        
+        unbilled_meter_event_obj = http.request.env['mgs.meter.event.blocking']
+        
+        collector_id = self.get_user_partner_id(request.session.uid)
+        
+        property_id = kw.get("property")
+        reason_id = kw.get("reason")        
+        
+        domain = [('name', '=', property_id.upper()), ('zone_id.collector_id.id', '=', collector_id), ('state', '=', 'connected')]        
+        
+        property_id = http.request.env['mgs_billing.property'].sudo().search(domain, limit=1)
+        
+        if not property_id:
+            return "Invalid Home Number or Home is not Connected."
+        try:
+            created_reading = unbilled_meter_event_obj.sudo().create({
+                'property_id': property_id.id,                
+                'reason_id': reason_id,                
+                'user_id': request.session.uid,                
+            })
+            
+            return {                
+                'response': 'Success'
+            }
+        
+        except Exception as e:
+            return {
+                "error": "ERROR: "+str(e),
+                'response': 'Fail'
+            }
+        
+          
+    def get_user_partner_id(self, user):
+        user_id = request.env['res.users'].search([('id', '=', user)])
+        return user_id.partner_id.id if user_id else None    
+        
+        
+    @http.route('/billingApi/meter_event_blocking_reasons', type='http', auth='public', methods=['GET'], csrf=False)
+    def get_meter_event_blocking_reasons(self, **kwargs):
+        try:
+            # Query your model
+            reasons = request.env['mgs.meter.event.blocking.reason'].sudo().search([])
+            
+            # Prepare data
+            data = [{'id': r.id, 'name': r.name} for r in reasons]
+
+            return request.make_response(
+                json.dumps({'status': 'success', 'data': data}),
+                headers=[('Content-Type', 'application/json')]
+            )
+        except Exception as e:
+            return request.make_response(
+                json.dumps({'status': 'error', 'message': str(e)}),
+                headers=[('Content-Type', 'application/json')]
+            )
+        
